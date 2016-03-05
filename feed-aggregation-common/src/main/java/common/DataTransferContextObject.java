b247085a21bed2aec3666843a635459d62dbdedd
Yvonne <yvonnechan0@gmail.com>
2016-03-05 12:48:40 -0500

    project structure

---
+++ feed-aggregation-common/src/main/java/common/DataTransferContextObject.java
@@ -0,0 +1,10 @@
+package common;
+
+import java.io.Serializable;
+
+/**
+ * Base interface for all Data Transfer Objects, used to identify DTOs
+ * Created by Yvonne on 2016-03-05.
+ */
+public interface DataTransferContextObject extends Serializable {
+}
